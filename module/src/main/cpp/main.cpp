95fc51700b662129c63e4024dd41ed3394bb1e6b
Rizalda <rizaldalfa@outlook.com>
2024-09-16 07:35:38 +0700

    Cleaning Code

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,22 +1,7 @@
-/* Copyright 2022-2023 John "topjohnwu" Wu
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
- * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
- */
-
 #include <cstdlib>
 #include <unistd.h>
 #include <fcntl.h>
 #include <android/log.h>
-
 #include "zygisk.hpp"
 
 using zygisk::Api;
@@ -24,6 +9,8 @@ using zygisk::AppSpecializeArgs;
 using zygisk::ServerSpecializeArgs;
 
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "MyModule", __VA_ARGS__)
+
+void Module();
 
 class MyModule : public zygisk::ModuleBase {
 public:
@@ -35,8 +22,10 @@ public:
     void preAppSpecialize(AppSpecializeArgs *args) override {
         // Use JNI to fetch our process name
         const char *process = env->GetStringUTFChars(args->nice_name, nullptr);
-        preSpecialize(process);
+        const char *data_dir = env->GetStringUTFChars(args->app_data_dir, nullptr);
+        preSpecialize(process, data_dir);
         env->ReleaseStringUTFChars(args->nice_name, process);
+        env->ReleaseStringUTFChars(args->app_data_dir, data_dir);
     }
 
     void preServerSpecialize(ServerSpecializeArgs *args) override {
@@ -77,3 +66,8 @@ static void companion_handler(int i) {
 // Register our module class and the companion handler function
 REGISTER_ZYGISK_MODULE(MyModule)
 REGISTER_ZYGISK_COMPANION(companion_handler)
+
+void Module()
+{
+  // TODO
+}
