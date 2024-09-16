3ed339bb5f45d3aa7e940bfce5461422474d05ea
Rizalda <rizaldalfa@outlook.com>
2024-09-16 14:27:36 +0700

    Update

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -3,14 +3,12 @@
 #include <fcntl.h>
 #include <android/log.h>
 #include "zygisk.hpp"
+#include <thread>
+#include <cstring>
 
 using zygisk::Api;
 using zygisk::AppSpecializeArgs;
 using zygisk::ServerSpecializeArgs;
-
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "MyModule", __VA_ARGS__)
-
-void Module();
 
 class MyModule : public zygisk::ModuleBase {
 public:
@@ -28,46 +26,18 @@ public:
         env->ReleaseStringUTFChars(args->app_data_dir, data_dir);
     }
 
-    void preServerSpecialize(ServerSpecializeArgs *args) override {
-        preSpecialize("system_server");
-    }
-
 private:
     Api *api;
     JNIEnv *env;
+    char* target_data_dir;
+    bool start_module;
 
-    void preSpecialize(const char *process) {
-        // Demonstrate connecting to to companion process
-        // We ask the companion for a random number
-        unsigned r = 0;
-        int fd = api->connectCompanion();
-        read(fd, &r, sizeof(r));
-        close(fd);
-        LOGD("process=[%s], r=[%u]\n", process, r);
-
-        // Since we do not hook any functions, we should let Zygisk dlclose ourselves
-        api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
+    void preSpecialize(const char* process, const char* data_dir) 
+    {
+        start_module = true;
     }
 
 };
 
-static int urandom = -1;
-
-static void companion_handler(int i) {
-    if (urandom < 0) {
-        urandom = open("/dev/urandom", O_RDONLY);
-    }
-    unsigned r;
-    read(urandom, &r, sizeof(r));
-    LOGD("companion r=[%u]\n", r);
-    write(i, &r, sizeof(r));
-}
-
-// Register our module class and the companion handler function
+// Register our module class
 REGISTER_ZYGISK_MODULE(MyModule)
-REGISTER_ZYGISK_COMPANION(companion_handler)
-
-void Module()
-{
-  // TODO
-}
