114acd8333b6c7d8669c3004f526883e5327b771
Rizalda <rizaldalfa@outlook.com>
2024-09-22 16:41:02 +0700

    Test game

--- module/src/main/cpp/module.cpp
+++ module/src/main/cpp/module.cpp
@@ -3,7 +3,7 @@
 
 void module(const char* target_data_dir)
 {
-    std::fstream log("/data/data/log/dump.txt", std::ios_base::app);
+    std::fstream log("/data/data/com.kiarygames.tinyroom/dump.txt", std::ios_base::app);
 
     log << "Hello World!" << std::endl;
 
